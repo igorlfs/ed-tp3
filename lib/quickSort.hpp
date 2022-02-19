176a20baf6c56e360e1f034420ce5f4a1569bb1e
Igor <igorlfs@ufmg.br>
2022-02-18 21:47:13 -0300

    chore: minor changes to quickSort

--- lib/quickSort.hpp
+++ lib/quickSort.hpp
@@ -1,31 +1,31 @@
+// Funções implementadas no header devido a idiossincrasias de templates em C++
 template <typename T>
-void partition(const int &left, const int &right, int *i, int *j, T *r) {
-    *i = left;
-    *j = right;
-
-    T pivot = r[(*i + *j) / 2];
+void partition(const int &left, const int &right, int &i, int &j, T *r) {
+    i = left;
+    j = right;
+    T pivot = r[(i + j) / 2];
 
     do {
-        while (pivot > r[*i])
-            (*i)++;
-        while (pivot < r[*j])
-            (*j)--;
+        while (pivot > r[i])
+            i++;
+        while (pivot < r[j])
+            j--;
 
-        if (*i <= *j) {
-            T aux = r[*i];
-            r[*i] = r[*j];
-            r[*j] = aux;
+        if (i <= j) {
+            T aux = r[i];
+            r[i] = r[j];
+            r[j] = aux;
 
-            (*i)++;
-            (*j)--;
+            i++;
+            j--;
         }
-    } while (*i <= *j);
+    } while (i <= j);
 }
 
 template <typename T> void sort(const int &left, const int &right, T *v) {
     int i, j;
 
-    partition(left, right, &i, &j, v);
+    partition(left, right, i, j, v);
 
     if (left < j) sort(left, j, v);
     if (i < right) sort(i, right, v);
