528217631cccdd4fd7ba5884cae81df38056415a
Igor <igorlfs@ufmg.br>
2022-02-18 21:08:39 -0300

    chore(linkedList): cleanup, doc

--- lib/linkedList.hpp
+++ lib/linkedList.hpp
@@ -4,9 +4,6 @@
 #include "linearList.hpp"
 #include <ostream>
 
-using std::ostream;
-using std::string;
-
 template <typename T> class LinkedList : public LinearList<T> {
   public:
     // Construtores
@@ -18,24 +15,14 @@ template <typename T> class LinkedList : public LinearList<T> {
     Cell<T> *getHead() { return this->head; }
 
     // Modificadores
-    void insertBeg(const T &u);
-    void insertEnd(const T &u);
-    void insertPos(const T &u, const int &pos);
+    void insertEnd(const T &c);
     T removeBeg();
     void clear() override;
-    /* void escalona(ostream &out, const int &n); */
-    /* void escalonaTudo(ostream &out); */
 
     // Lookup
-    bool find(const T &str) const;
-
-    // Operações
-    /* void print(ostream &out) const; */
+    bool find(const T &t) const;
 
   private:
     Cell<T> *head;
     Cell<T> *tail;
-
-    // Setters
-    Cell<T> *setPos(const int &pos, const bool &before) const;
 };
--- src/linkedList.cpp
+++ src/linkedList.cpp
@@ -1,10 +1,6 @@
 #include "linkedList.hpp"
 #include <iostream>
 
-using std::cout;
-using std::endl;
-using std::pair;
-
 // @brief Constrói lista inicializando cabeça e cauda
 template <typename T> LinkedList<T>::LinkedList() : LinearList<T>() {
     this->head = new (std::nothrow) Cell<T>;
@@ -18,61 +14,19 @@ template <typename T> LinkedList<T>::~LinkedList() {
     delete this->head;
 }
 
-// @brief Retorna a célula (antes) na posição pos
-// @param pos, before (se quero a posição antes, assume-se falso)
-template <typename T>
-Cell<T> *LinkedList<T>::setPos(const int &pos, const bool &before) const {
-    Cell<T> *p = this->head;
-    for (int i = 1; i < pos; ++i)
-        p = p->next;
-
-    if (!before) p = p->next;
-
-    return p;
-}
-
-// @brief insere uma nova célula no começo da lista
-// @param u, URL a ser inserida
-template <typename T> void LinkedList<T>::insertBeg(const T &u) {
-    Cell<T> *newCell = new (std::nothrow) Cell<T>;
-    /* erroAssert(newCell, "Falha ao alocar dinamicamente a célula."); */
-
-    newCell->item = u;
-    newCell->next = this->head->next;
-    this->head->next = newCell;
-    this->tail = newCell;
-    this->size++;
-}
-
 // @brief insere uma nova célula no final da lista
-// @param u, URL a ser inserida
-template <typename T> void LinkedList<T>::insertEnd(const T &u) {
+// @param c, célula a ser inserida
+template <typename T> void LinkedList<T>::insertEnd(const T &c) {
     Cell<T> *newCell = new (std::nothrow) Cell<T>;
 
-    newCell->item = u;
+    newCell->item = c;
     this->tail->next = newCell;
     this->tail = newCell;
     this->size++;
 }
 
-// @brief insere uma nova célula numa dada posição da lista
-// @param u, URL a ser inserida, pos, posição de inserção
-template <typename T>
-void LinkedList<T>::insertPos(const T &u, const int &pos) {
-    Cell<T> *newCell = new (std::nothrow) Cell<T>;
-
-    Cell<T> *p = setPos(pos, true);
-
-    newCell->item = u;
-    newCell->next = p->next;
-    p->next = newCell;
-    this->size++;
-
-    if (newCell->next == nullptr) this->tail = newCell;
-}
-
 // @brief remove a célula no começo da lista
-// @return URL removida
+// @return item da célula removida
 template <typename T> T LinkedList<T>::removeBeg() {
 
     /* erroAssert(!this->empty(), "Falha ao remover item da lista: lista
@@ -104,8 +58,8 @@ template <typename T> void LinkedList<T>::clear() {
 }
 
 // @brief verifica se a lista contém a URL
-// @param str, URL procurada
-// @return true se URL está na lista e false caso contraŕio
+// @param t, item procurado
+// @return true se item estiver na lista e false caso contraŕio
 template <typename T> bool LinkedList<T>::find(const T &t) const {
     Cell<T> *p = this->head->next;
 
@@ -118,7 +72,8 @@ template <typename T> bool LinkedList<T>::find(const T &t) const {
     return false;
 }
 
-template class LinkedList<string>;
-template class LinkedList<double>;
+// Defina aqui as instâncias usadas no programa
+// (Idiossincrasia de C++ com templates)
+template class LinkedList<std::string>;
 template class LinkedList<int>;
-template class LinkedList<pair<string, int>>;
+template class LinkedList<std::pair<std::string, int>>;
